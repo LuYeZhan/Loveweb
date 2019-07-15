58ef464cd8eb7f7eec546934456ef970a50bc40e
Lu <lu@hooli.store>
2019-07-15 12:06:51 +0200

    landing page refactorizada

--- pages/Landingpage.js
+++ pages/Landingpage.js
@@ -1,21 +1,20 @@
 'use strict';
 
-function LandingPage(parentElement){
-  this.parentElement = parentElement;
-  this.elements = null;
-}
-
-LandingPage.prototype.generate = function(){
-  this.elements = `
+class LandingPage {
+  constructor(parentElement){
+    this.parentElement = parentElement;
+    this.elements = null;
+  }
+  generate(){
+    this.elements = `
     <header>
     <h1>Lovapp</h1>
     <video id="media" src="./videos/intro.mp4" muted="muted" autoplay="autoplay" loop></video>
     </header>
   `;
-  this.render();
-
-}
-
-LandingPage.prototype.render = function(){
-  this.parentElement.innerHTML = this.elements;
+    this.render();
+  }
+  render(){
+    this.parentElement.innerHTML = this.elements;
+  }
 }
