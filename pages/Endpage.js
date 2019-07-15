a91f759400ca888f1497c4ae864c8b4fd8ee14a7
Lu <lu@hooli.store>
2019-07-15 12:02:43 +0200

    endpage refactorizado

--- pages/Endpage.js
+++ pages/Endpage.js
@@ -1,50 +1,49 @@
 'use strict';
 
-function EndPage(parentElement){
-  this.parentElement = parentElement;
-  this.elements = null;
-  this.users= null;
-  this.loading = null;
-}
-
-EndPage.prototype.generate = async function(){
-  this.loading = new Loading(this.parentElement);
-  this.loading.generate();
-  await this.connectToAPI();
-  this.elements = `
-    <header>
-      <h2>I'ts a Match!</h2>
-      <video id="media" src="./videos/end.mp4" muted="muted" autoplay="autoplay" loop></video>
-    </header>
-    <section class="endpage-section">
-  `;
-  this.users.forEach((users) => {
-    if (users.dob.age > 35){
-     users.dob.age = Math.floor(Math.random() * 8 + 25);
-    };
-    this.elements += `
-     
-        <h3>${users.name.title} ${users.name.first} ${ users.name.last}</h3>
-        <div><img src="${users.picture.large}"></img></div>
-        <p class="top-p">${users.gender}</p>
-        <p>${users.dob.age}</p>
-        <p>${users.nat}</p>
-        <p>${users.location.street}</p>
-        <p>${users.location.city}</p>
-        <p class="phone">${users.email}</p>
-        <p class="phone">${users.cell}</p>
-      
-    `;
-  })
-  this.elements += `
-  </section>`
-  this.render();
-}
-
-EndPage.prototype.render = function(){
-  this.parentElement.innerHTML = this.elements;
-}
-
-EndPage.prototype.connectToAPI = async function() {
-  this.users= await loveServiceInstance.getAllUsers();
+class EndPage{
+  constructor(parentElement){
+    this.parentElement = parentElement;
+    this.elements = null;
+    this.users= null;
+    this.loading = null;
+    }
+    async generate(){
+      this.loading = new Loading(this.parentElement);
+      this.loading.generate();
+      await this.connectToAPI();
+      this.elements = `
+        <header>
+          <h2>I'ts a Match!</h2>
+          <video id="media" src="./videos/end.mp4" muted="muted" autoplay="autoplay" loop></video>
+        </header>
+        <section class="endpage-section">
+      `;
+      this.users.forEach((users) => {
+        if (users.dob.age > 35){
+         users.dob.age = Math.floor(Math.random() * 8 + 25);
+        };
+        this.elements += `
+         
+            <h3>${users.name.title} ${users.name.first} ${ users.name.last}</h3>
+            <div><img src="${users.picture.large}"></img></div>
+            <p class="top-p">${users.gender}</p>
+            <p>${users.dob.age}</p>
+            <p>${users.nat}</p>
+            <p>${users.location.street}</p>
+            <p>${users.location.city}</p>
+            <p class="phone">${users.email}</p>
+            <p class="phone">${users.cell}</p>
+          
+        `;
+      })
+      this.elements += `
+      </section>`
+      this.render();
+    }
+    render () {
+      this.parentElement.innerHTML = this.elements;
+    }
+    async connectToAPI() {
+      this.users= await loveServiceInstance.getAllUsers();
+    }
 }
